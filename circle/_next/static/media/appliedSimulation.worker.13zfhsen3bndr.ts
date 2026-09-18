6d143c615646f123a07d9abffd47c2ff1d67abaa
IvicaSkrobo <IvicaSkrobo@users.noreply.github.com>
2026-09-18 09:13:51 +0200

    Publish Circle over-cap path reporting

--- circle/_next/static/media/appliedSimulation.worker.13zfhsen3bndr.ts
+++
@@ -1,9 +0,0 @@
-import { simulateAppliedPool } from './appliedSimulation'
-
-self.onmessage = ({ data }) => {
-  try {
-    self.postMessage({ result: simulateAppliedPool(data.report, 100000, 42, progress => self.postMessage({ progress })) })
-  } catch (error) {
-    self.postMessage({ error: String(error) })
-  }
-}
