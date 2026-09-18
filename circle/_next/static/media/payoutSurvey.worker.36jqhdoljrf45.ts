9b136b7fa3a6e185cd23c7ac95a5f28a5350b611
IvicaSkrobo <IvicaSkrobo@users.noreply.github.com>
2026-09-18 14:49:55 +0200

    Publish Circle Classic and Daredevil cues

--- circle/_next/static/media/payoutSurvey.worker.36jqhdoljrf45.ts
+++ circle/_next/static/media/payoutSurvey.worker.36jqhdoljrf45.ts
@@ -1,5 +1,5 @@
 import { runPayoutSurvey } from './payoutSurvey'
 self.onmessage = ({data}) => {
-  try { self.postMessage({report:runPayoutSurvey(data.zones,data.samples,n=>self.postMessage({progress:n}))}) }
+  try { self.postMessage({report:runPayoutSurvey(data.zones,data.samples,n=>self.postMessage({progress:n}),data.start??0)}) }
   catch(error) { self.postMessage({error:String(error)}) }
 }
